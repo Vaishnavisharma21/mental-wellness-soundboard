1a34441c1c6ecedbf3b9a273c3d4acc6dfc914ac
Your Name <you@example.com>
2026-03-31 04:12:26 +0530

    My first commit by Agrima

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import { Server } from "socket.io";
+import { createServer } from 'node:http';
+import express from 'express';
+
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(4600, () => {
+  console.log('server running at http://localhost:4600');
+});
